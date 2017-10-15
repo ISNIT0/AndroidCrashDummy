21e458a4299d7b00a561d6b9ebb3bbdd4c42c37a
Joseph Reeve <joseph.reeve@googlemail.com>
2017-10-15 14:29:09 +0100

    Moved log test logic into an external library

--- app/src/androidTest/java/com/example/user/androidtestapp/LogTest.java
+++ app/src/androidTest/java/com/example/user/androidtestapp/LogTest.java
@@ -1,6 +1,7 @@
 package com.example.user.androidtestapp;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -20,33 +21,40 @@ import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+import xyz.reeve.logassert.LogAssert;
+
 import static org.junit.Assert.assertEquals;
 
 
 @RunWith(AndroidJUnit4.class)
 public class LogTest {
-
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule =
             new ActivityTestRule(MainActivity.class);
 
     @Test
     public void clickLog100Button() throws Exception {
-
-        clearLog(); // Otherwise our logcat command could show previous test-runs etc.
-        onView(withId(R.id.log100)).perform(click()); // UI Action / Trigger logs
-        String log = getLogs(); // Get all logs in logcat buffer for this app (could have logs from parallel executions, not just the line above)
-
-        Log.d("LogTest", "Output from log reading has length: [" + log.length() + "]");
-
-        Pattern mPattern = Pattern.compile("Logging 100 messages took [0-9]+ Milliseconds");
-
-        Matcher matcher = mPattern.matcher(log);
-        if(!matcher.find()) {
-            throw new Exception("Logging was not detected!");
-        } else {
-            Log.i("LogTest", "ClickLog100Button Passed");
+        String permission = "android.permission.WRITE_EXTERNAL_STORAGE";
+        int res = mActivityRule.getActivity().getApplicationContext().checkCallingOrSelfPermission(permission);
+        if (res != PackageManager.PERMISSION_GRANTED) {
+            throw new Exception("App does not have permission [android.permission.READ_LOGS]");
         }
+
+        LogAssert logAssert = new LogAssert() {
+            @Override
+            public void customHandler(String logs) throws Exception {
+                Log.d("LogAssert", "Custom Log Handler");
+            }
+        };
+
+        onView(withId(R.id.log100)).perform(click()); // UI Action / Trigger logs
+
+        //Log.d("LogTest", "Output from log reading has length: [" + log.length() + "]");
+
+        String[] assertArr = {"Logging 100 messages took [0-9]+ Milliseconds"};
+        logAssert.assertLogsExist(assertArr);
+        logAssert.assertLogsCustom();
     }
 
 
